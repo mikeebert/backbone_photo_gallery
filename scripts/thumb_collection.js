cfa17fea35d81c0fec15d14ddc417d28b33227f5
Mike Ebert <mike@mikeebert.net>
2014-04-05 09:27:09 -0500

    Makes deselection more explicit

--- scripts/thumb_collection.js
+++ scripts/thumb_collection.js
@@ -2,15 +2,19 @@ var Thumbs = Backbone.Collection.extend({
   model: Thumb,
 
   select: function(model) {
-    if( this.selectedThumb() ) {
-      this.selectedThumb().select(false);
-    };
+    this.deselectCurrentThumb();
+    model.set('select', true);
     this.selected = model;
-    this.selected.select(true);
     this.trigger('thumbs:selected');
   },
 
   selectedThumb: function() {
     return this.selected;
+  },
+
+  deselectCurrentThumb: function() {
+    if( this.selectedThumb() ) {
+      this.selectedThumb().set('select', false);
+    };
   }
 });
